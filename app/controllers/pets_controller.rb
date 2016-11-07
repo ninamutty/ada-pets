cc0b1a8aa8062e3aad99d845442e9000aab36ded
Nina Mutty <ninamutty@gmail.com>
2016-11-07 14:12:04 -0800

    simple api

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -1,5 +1,29 @@
 class PetsController < ApplicationController
   def index
-    @pets = Pet.all
+    pets = Pet.all
+
+    #pets = Pet.select(:name, :id, :human, :age)
+    # render :json => pets.as_json(:only => [:name, :id, :human, :age])
+    render :json => pets.as_json(:only => [:name, :id, :human, :age], :status => :ok)
+  end
+
+  def show
+    ## Getting a specific pet back
+    pet = Pet.find_by(id: params[:id])
+
+    #if pet
+      render :json => pet, :only => [:name, :id, :human, :age], :status => :ok
+    # else
+    #   render :json => [], :status => :no_content
+    # end
+  end
+
+  def search
+    pets = Pet.where(name: params[:query].capitalize)
+    unless pets.empty?
+      render :json => pet, :only => [:name, :id, :human, :age], :status => :ok
+    else
+      render :json => [], :status => :no_content
+    end
   end
 end
--- test/controllers/pets_controller_test.rb
+++ test/controllers/pets_controller_test.rb
@@ -37,4 +37,40 @@ class PetsControllerTest < ActionController::TestCase
     body = JSON.parse(response.body)
     assert_equal keys, body.map(&:keys).flatten.uniq.sort
   end
+
+  test "can get #show" do
+    get :show, {id: pets(:one).id}
+    assert_response :ok
+  end
+
+  test "#show returns json" do
+    get :show, {id: pets(:one).id}
+    assert_match 'application/json', response.header['Content-Type']
+  end
+
+  test "#show returns an Hash of a Pet" do
+    get :show, {id: pets(:one).id}
+    # Assign the result of the response from the controller action
+    body = JSON.parse(response.body)
+    assert_instance_of Hash, body
+  end
+
+  test "returns one pet objects with 4 key-values" do
+    get :show, {id: pets(:one).id}
+    body = JSON.parse(response.body)
+    assert_equal 4, body.length
+  end
+
+  test "the one pet object contains the relevant keys" do
+    keys = %w( age human id name )
+    get :show, {id: pets(:one).id}
+    body = JSON.parse(response.body)
+    assert_equal keys, body.keys.sort
+  end
+
+  test "returns status no-content if not found" do
+    get :show, {id: 134354}
+    body = JSON.parse(response.body)
+    assert_response :no_content
+  end
 end
